0d985e42e941ad1137091ff58f7d34781941859e
KevinSolimo <kevinsolimo@gmail.com>
2019-04-13 15:43:53 +0000

    Esercizio 15.2 Completo

--- AngularHttp/src/app/app.module.ts
+++ AngularHttp/src/app/app.module.ts
@@ -3,12 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http'; //Importa Il modulo http
+
+import {MatButtonModule} from '@angular/material';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatListModule} from '@angular/material/list';
+
+import { FooComponent } from './foo/foo.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule,
+    MatButtonModule, MatProgressSpinnerModule, MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
